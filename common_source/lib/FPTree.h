2b6d4e8e5fd2988bee171b24f0551b5c1986057c
Quoc Huy Nguyen <lawrencealgorithm@gmail.com>
2018-03-25 12:07:48 +0700

    Add class FPTree

---
+++ common_source/lib/FPTree.h
@@ -0,0 +1,16 @@
+#ifndef FPTREE_INCLUDED
+#define FPTREE_INCLUDED
+
+#include "BangBit.h"
+
+namespace NguyenQuocHuy {
+    class FPTree {
+    private:
+    protected:
+        BangBit* csdl;
+    public:
+        FPTree(BangBit* coSoDuLieu);
+    };
+}
+
+#endif
